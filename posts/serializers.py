2d2db52d0197038461bde4a8bb12896ed848df99
Monika Jakobsson <nimrin81@gmail.com>
2022-07-05 13:07:50 +0000

    Add following data to profiles view and likes data to posts view

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from likes.models import Like
 from .models import Post
 
 
@@ -7,6 +8,7 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024:
@@ -27,10 +29,19 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'subtitle', 'content', 'image'
+            'title', 'subtitle', 'content', 'image', 'like_id'
         ]
