50cac107bddfebaca6b0eecca1e73d31fe14cabf
Monika Jakobsson <nimrin81@gmail.com>
2022-07-05 13:43:53 +0000

    Install django-filter and create filtering methods for posts, profiles and comments

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from wanderlust_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -12,7 +14,30 @@ class PostList(generics.ListCreateAPIView):
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        comments_count=Count('comment', distinct=True),
+        likes_count=Count('likes', distinct=True)
+    ).order_by('-created_at')
+
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+    ]
+    search_fields = [
+        'owner__username',
+        'title'
+    ]
+    ordering_fields = [
+        'comments_count',
+        'likes_count',
+        'likes__created_at',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -23,6 +48,9 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     Displays single post. If the user is the post owner;
     allows editing and deleting of post
     '''
-    permission_classes = [IsOwnerOrReadOnly]
     serializer_class = PostSerializer
-    queryset = Post.objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Post.objects.annotate(
+                   comments_count=Count('comment', distinct=True),
+                   likes_count=Count('likes', distinct=True)
+               ).order_by('-created_at') 
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from wanderlust_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -9,16 +10,20 @@ class ProfileList(generics.ListAPIView):
     '''
     Listview to display profiles
     '''
+    serializer_class = ProfileSerializer
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
         followers_count=Count('owner__followed', distinct=True),
         following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
-    
-    serializer_class = ProfileSerializer
 
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        DjangoFilterBackend
+    ]
+
+    filterset_fields = [
+        'owner__following__followed__profile'
     ]
 
     ordering_fields = [
@@ -34,11 +39,10 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     Displays single profile. If the user is the profile owner;
     allows editing of profile.
     '''
+    serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
         followers_count=Count('owner__followed', distinct=True),
         following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
-
-    serializer_class = ProfileSerializer
