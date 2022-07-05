37b33471cd9d24acb0e05ec665c9ccca926f5678
Monika Jakobsson <nimrin81@gmail.com>
2022-07-05 14:49:04 +0000

    install psycopg2

--- wanderlust_api/settings.py
+++ wanderlust_api/settings.py
@@ -72,7 +72,6 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 REST_FRAMEWORK = {
-    'DATETIME_FORMAT': '%d %b %Y',
     'DEFAULT_AUTHENTICATION_CLASSES': [(
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
@@ -80,7 +79,8 @@ REST_FRAMEWORK = {
         )],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
-        'PAGE_SIZE': 10,
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y',
     }
 
 if 'DEV' not in os.environ:
--- wanderlust_api/views.py
+++ wanderlust_api/views.py
@@ -1,14 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE, 
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
     JWT_AUTH_SECURE,
 )
 
 
 @api_view()
 def root_route(request):
-    return Response({"message": "Hi and welcome to my django rest framework API!"})
+    return Response({
+        "message": "Hi and welcome to my django rest framework API!"
+    })
+
 
 # dj-rest-auth logout view fix: code credit @ Code Institute LMS.
 @api_view(['POST'])
